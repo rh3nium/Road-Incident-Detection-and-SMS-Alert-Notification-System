4567b362ae5baffd43cecdef3f6a4b14320593d4
Riya H <riyahal.dev@gmail.com>
2025-11-16 14:32:00 +0530

    Delete resources.py

--- resources.py
+++
@@ -1,9 +0,0 @@
-# resources.py
-
-# Mapping of resource type to default receiver phone numbers
-RESOURCE_RECEIVERS = {
-    "Ambulance": ["no."],
-    "Fire Truck": ["no.", "no."],
-    "Police": ["no.", "no."],
-}
-
